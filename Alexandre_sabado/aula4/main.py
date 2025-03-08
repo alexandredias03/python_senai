fc1955193f5b405c9142684c444721e592730a44
Alexandre Dias <113298362+alexandredias03@users.noreply.github.com>
2025-03-08 09:27:48 -0300

    Add files via upload

---
+++ Alexandre_sabado/aula4/main.py
@@ -0,0 +1,27 @@
+from src.controller import produto_controller # type: ignore
+
+def exibir_menu():
+    print("\nMAREA TOCA TUDO LTDA!")
+    print("\n==== MENU ====")
+    print("1 - Cadastrar Produto")
+    print("2 - Listar Produto")
+    print("3 - Atualizar Produto")
+    print("4 - Deletar Produto")
+    print("5 - Buscar produto unico")
+    print("0 - Sair")
+
+def listar_produtos():
+    print("\n --- Lista de Produtos --- ")  
+    produtos = produto_controller.listar_produtos()  
+    if produtos:
+        for produto in produtos:
+            print(f"ID: {produto['id']}, Nome: {produto['nome']}, Preco: {produto['preco']}")
+    else:
+        print("Não existe produtos cadastrados")                
+
+def cadastrar_produto():
+    print("\n --- Cadastrar produto --- ")
+    nome = input("Digite o nome: ")
+    preco = input("Digite o preco")
+    novo_id = produto_controller.cadastrar_produto(nome, preco)
+    print(f"Produto cadastrado com sucesso com o novo ID")    
